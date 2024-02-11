496799d5c62735e904ce8a2f6457ada028dafcca
zezo1422 <hdmyhd333@gmail.com>
2024-02-11 17:41:17 +0300

    first commit

---
+++ src/main/java/com/arjuncodes/hello/HelloController.java
@@ -0,0 +1,16 @@
+package com.arjuncodes.hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    
+    public String hello(){
+        return "Hello World";
+    }
+    
+}
